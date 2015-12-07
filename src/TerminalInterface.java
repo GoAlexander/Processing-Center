ad67b82f96e1d7188701ecf72cb328ef3499e1cf
KsushaKostrova <KsuhaKostrova@yandex.ru>
2015-12-07 20:04:48 +0300

    //примерное содержание терминала

--- src/TerminalInterface.java
+++ src/TerminalInterface.java
@@ -1,20 +1,106 @@
+public class Terminal {
+	private Card card;
+	private int sum;
+	private int triesLimit;
+	private boolean nextOperation;
+	private boolean commitAnOperation;
 
-//Класс для интерфейса платежного терминала
-//(task #2+3)
-public class TerminalInterface {
+	public Terminal(InterfClass a) {
 
-	void dataCardInput(){
+		card.cardNum = a.someCard.cardNum;
+		triesLimit = 3;
+		nextOperation = true;
+		commitAnOperation = true;
+		try {
+
+			if (nextOperation == false)
+				throw new Exception();
+			pinInput(a);
+			checkValidity();
+			checkIfNotInTheStopList();
+			sum = a.sumOfOperationInput();
+			proofOfAuthorization();
+
+		} catch (Exception e) {
+			commitAnOperation = false;
+		}
+
 	}
-	void pinInput(){
+
+	// сравнение с базой
+	// real- реальный пин-код карты
+	void pinInput(InterfClass a) {
+		// из бд
+		int real = 0;// поиск по номеру карты
+		for (int i = 0; i < triesLimit; i++) {
+			a.interfPinCode();
+			if (a.someCard.pin == real) {
+				card.pin = a.someCard.pin;
+				break;
+			} else if (i == 2) {
+				// вывод в интерфейс
+				System.out.println("No more tries.Your card will be blocked for one day");
+				nextOperation = false;
+			} else
+				System.out.println("incorrect pin-code");
+		}
 	}
-	void sumOfOperationInput(){
+
+	private boolean checkValidity() {
+		boolean res = false;
+		if (commitAnOperation == true) {
+			// card.validity=;//срок действия карты из бд
+			if (card.validity == false)
+				nextOperation = false;
+		}
+		return res;
 	}
-	void checkPrinting(){
+	
+	private boolean checkIfNotInTheStopList(){
+		if (commitAnOperation == true);
+		//	проверка по базе
+		return false;
 	}
-	void proofOfAuthorization(){
+
+	void commitAuthorization() {
+		if (proofOfAuthorization() == true)
+			authorizationRequest();
+		else
+			;// совершить транзакцию без авторизации
+			//совершать сразу здесь?
+			//предполагается, что позже данные будут изменены (остаток по счету)
 	}
-	void receivingProcessingOfAnswer(){
+
+	// сравнение с данными бд. проверка, нужна ли авторизация вообще
+	boolean proofOfAuthorization() {
+		boolean res = false;
+		int DBSum = 0;// для этого нужен номер карты
+		if (commitAnOperation == true) {
+			if (sum <= DBSum)
+				res = false;
+		}
+		return res;
 	}
-	void authorizationRequest(){
+
+	void authorizationRequest() {//какие параметры передавать?
+		if (commitAnOperation == true)
+			receivingProcessingOfAnswer();
+	}
+
+	void receivingProcessingOfAnswer() {//нужны ли параметры?
+		if (commitAnOperation == true)
+			;
+	}
+
+	// вывод чека в интерфейсе
+	void checkPrinting() {
+		if (commitAnOperation == true)
+			;
+
+	}
+
+	public static void main(String[] args) {
+		InterfClass someInterf=new InterfClass();
+		Terminal t = new Terminal(someInterf);
 	}
 }
