1c120c5b6046bc770ce343394fb1e00f55998697
SokolovaNastya <xxxhactyaxxx@mail.ru>
2015-12-07 21:39:59 +0300

    Задание 8
    Реализовала все функции, которые были в скелете. Еще изменила немного
    класс UserDataBase.

--- src/FrontOffice.java
+++ src/FrontOffice.java
@@ -1,4 +1,4 @@
-//Класс, в котором все методы относящиеся к Фронт Офису
+﻿//Класс, в котором все методы относящиеся к Фронт Офису
 
 public class FrontOffice {
 	//structures 
@@ -11,11 +11,14 @@ public class FrontOffice {
 	
 	//"структура" юзера, здесь хранится вся инфа, которая должна храниться во Фронт Офисе
 	class UserDataBase { //нужно немного отредактировать с учётом задания
-		int pin;
-		long deadline_of_card;
+		//int pin;
+		//long deadline_of_card;
 		Sum oper_sum;
-		boolean stop_listl; //0 -> in the stop list
+		//boolean stop_listl; //0 -> in the stop list
 		//TODO что-то ещё?
+		String numCard;
+		String pinCode;
+		String desireSum;
 	}
 	
 	//Купюры
@@ -46,27 +49,150 @@ public class FrontOffice {
 	//Обработка транзакций (task #8)
 	//Transaction control. Begin.
 	//true -> означает, что метод выполнен успешно, всё правильно
-	boolean pinChecking(){
-		return true;
-	}
-	boolean usefulTimeOfCardChecking(){
-		return true;
-	}
-	boolean moneyOnCardCheckin(){
-		return true;
-	}
-	boolean cardInStoplistChecking(){
-		return true;
-	}
-	//Больше проверок??? (методов)
-	
-	boolean transactionControl(UserDataBase  user_data)
-	{
-		if( pinChecking()==true && usefulTimeOfCardChecking()==true && moneyOnCardCheckin()==true && cardInStoplistChecking()==true )
-		{
-			return true;
+	public static SimpleDateFormat data = new SimpleDateFormat("yyyy/MM/dd");
+
+	int findRowUser(String numCard) throws IOException {
+		FileInputStream fis = new FileInputStream("C:/Users/Артём/Desktop/test.xls");
+		Workbook wb = new HSSFWorkbook(fis);
+		int row = 0;
+		for (int i = 0;; i++) {
+			if (numCard.equals(getCellText(wb.getSheetAt(0).getRow(i).getCell(0)))) {
+				row = i;
+				break;
+			}
 		}
-		else
+		fis.close();
+		return row;
+	}
+
+	int findColUser(String phrase) throws IOException {
+		FileInputStream fis = new FileInputStream("C:/Users/Артём/Desktop/test.xls");
+		Workbook wb = new HSSFWorkbook(fis);
+		int col = 0;
+		for (int i = 0;; i++) {
+			if (phrase.equals(getCellText(wb.getSheetAt(0).getRow(0).getCell(i)))) {
+				col = i;
+				break;
+			}
+		}
+		fis.close();
+		return col;
+	}
+
+	boolean pinChecking() throws IOException {
+		UserDataBase user = new UserDataBase();
+		FileInputStream fis = new FileInputStream("C:/Users/Артём/Desktop/test.xls");
+		Workbook wb = new HSSFWorkbook(fis);
+		int row = findRowUser(user.numCard);
+		int col = findColUser("Пин код");
+		if (user.pinCode.equals(getCellText(wb.getSheetAt(0).getRow(row).getCell(col)))) {
+			fis.close();
+			return true;
+		} else {
+			fis.close();
+			return false;
+		}
+	}
+
+	boolean usefulTimeOfCardChecking() throws IOException {
+		UserDataBase user = new UserDataBase();
+		FileInputStream fis = new FileInputStream("C:/Users/Артём/Desktop/test.xls");
+		Workbook wb = new HSSFWorkbook(fis);
+		int row = findRowUser(user.numCard);
+		int col = findColUser("Срок карты");
+		Date d = new Date();
+		String factData = getCellText(wb.getSheetAt(0).getRow(row).getCell(col));
+		String currData = data.format(d);
+		String factDataArr[] = factData.split("/");
+		String currDataArr[] = currData.split("/");
+		if ((Integer.valueOf(currDataArr[0]).compareTo(Integer.valueOf(factDataArr[0])) == -1)) {
+			fis.close();
+			return true;
+		} else if (Integer.valueOf(currDataArr[0]).compareTo(Integer.valueOf(factDataArr[0])) == 0) {
+			if (Integer.valueOf(currDataArr[1]).compareTo(Integer.valueOf(factDataArr[1])) == -1) {
+				fis.close();
+				return true;
+			} else if (Integer.valueOf(currDataArr[1]).compareTo(Integer.valueOf(factDataArr[1])) == 0) {
+				if ((Integer.valueOf(currDataArr[2]).compareTo(Integer.valueOf(factDataArr[2])) == -1)
+						|| (Integer.valueOf(currDataArr[2]).compareTo(Integer.valueOf(factDataArr[2])) == 0)) {
+					fis.close();
+					return true;
+				} else {
+					fis.close();
+					return false;
+				}
+			} else {
+				fis.close();
+				return false;
+			}
+		} else {
+			fis.close();
+			return false;
+		}
+	}
+
+	boolean moneyOnCardChecking() throws IOException {
+		UserDataBase user = new UserDataBase();
+		FileInputStream fis = new FileInputStream("C:/Users/Артём/Desktop/test.xls");
+		Workbook wb = new HSSFWorkbook(fis);
+		int row = findRowUser(user.numCard);
+		int col = findColUser("Остаток");
+		Integer desSum = Integer.valueOf(user.desireSum);
+		Integer factSum = (int) wb.getSheetAt(0).getRow(row).getCell(col).getNumericCellValue();
+		if ((desSum.compareTo(factSum) == -1) || (desSum.compareTo(factSum) == 0)) {
+			fis.close();
+			return true;
+		} else {
+			fis.close();
+			return false;
+		}
+	}
+
+	boolean cardInStoplistChecking() throws IOException {
+		UserDataBase user = new UserDataBase();
+		FileInputStream fis = new FileInputStream("C:/Users/Артём/Desktop/test.xls");
+		Workbook wb = new HSSFWorkbook(fis);
+		int row = findRowUser(user.numCard);
+		int col = findColUser("Стоп лист");
+		if ((int) wb.getSheetAt(0).getRow(row).getCell(col).getNumericCellValue() == 0) {
+			fis.close();
+			return true;
+		} else {
+			fis.close();
+			return false;
+		}
+	}
+
+	public static String getCellText(Cell cell) {
+		String result = "";
+		switch (cell.getCellType()) {
+		case Cell.CELL_TYPE_STRING:
+			result = cell.getRichStringCellValue().getString();
+			break;
+		case Cell.CELL_TYPE_NUMERIC:
+			if (DateUtil.isCellDateFormatted(cell)) {
+				result = data.format(cell.getDateCellValue());
+			} else {
+				result = Integer.toString((int) cell.getNumericCellValue());
+			}
+			break;
+		case Cell.CELL_TYPE_BOOLEAN:
+			result = Boolean.toString(cell.getBooleanCellValue());
+			break;
+		case Cell.CELL_TYPE_FORMULA:
+			result = cell.getCellFormula();
+			break;
+		default:
+			break;
+		}
+		return result;
+	}
+
+	boolean transactionControl() throws IOException {
+		if ((pinChecking() == true) && (usefulTimeOfCardChecking() == true) && (moneyOnCardChecking() == true)
+				&& (cardInStoplistChecking() == true)) {
+			return true;
+		} else
 			return false;
 	}
 	//Transaction control. End.
