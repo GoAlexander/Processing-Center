a696f2bf8d5132adbfa7c17d937e3809a14d6766
KhAngelina <angelx19@mail.ru>
2015-12-11 19:25:40 +0300

    Update ATM Interface
    ATM Interface

--- src/ATM.java
+++ src/ATM.java
@@ -4,16 +4,22 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.swing.JFrame;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dialog.ModalExclusionType;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.border.TitledBorder;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -21,24 +27,32 @@ import javax.swing.JOptionPane;
 import java.awt.SystemColor;
 import java.awt.Window;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
+// Интерфейс банкомата (задание 4 Харчевникова А)
 public class ATM {
 
+	private static int MIN_SUM = 100;
+	private static int MAX_SUM = 50000;
 	private JFrame MainATM;
 	private JPanel panelAuthorisation;
-	private JTextField textFieldCardNumber;
+	private JTextField textFieldCardNumber, textFieldSum;
 	private JPanel panelCardNumber, panelPinNumbers, pinPadPanel, panelOperations;
 
 	private JPasswordField passwordField;
 	private JButton enterButton, clearButton, zeroButton, buttonSuccess;
 
+	private JButton buttonClearOperation, btnDrawOperation, btnCashOperation, btnBalance, buttonClear;
+
 	private String inputNumberCard = ""; // номер карты
 	private String input = "";// пин-код
 
+	JScrollPane scrollPaneInform, scrollPaneResult;
+	JTextArea textAreaOut, textAreaOutPut, textAreaResult;
+
 	private JButton[] numberButtons = new JButton[9];
 
-	private static int counter = 2;
-	
+	private static int counter = 3;
 
 	/**
 	 * Launch the application.
@@ -73,20 +87,16 @@ public class ATM {
 		MainATM.setBounds(100, 100, 437, 433);
 		MainATM.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		MainATM.getContentPane().setLayout(null);
+
 		
-		panelOperations = new JPanel();
-		panelOperations.setBounds(0, 0, 421, 395);
-		MainATM.getContentPane().add(panelOperations);
-		panelOperations.setLayout(null);
-		
-		JScrollPane scrollPaneInform = new JScrollPane();
-		scrollPaneInform.setBounds(37, 27, 344, 208);
-		panelOperations.add(scrollPaneInform);
 
 		panelAuthorisation = new JPanel();
-		panelAuthorisation.setBounds(0, 0, 545, 395);
+		panelAuthorisation.setBounds(0, 0, 421, 405);
 		MainATM.getContentPane().add(panelAuthorisation);
 		panelAuthorisation.setLayout(null);
+		
+		//panelAuthorisation.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		//panelAuthorisation.setMaximumSize(new Dimension(1100, 700));
 
 		panelCardNumber = new JPanel();
 		panelCardNumber.setBorder(new TitledBorder(null,
@@ -97,6 +107,7 @@ public class ATM {
 		panelCardNumber.setLayout(null);
 
 		textFieldCardNumber = new JTextField();
+
 		textFieldCardNumber.setBounds(22, 28, 301, 23);
 		panelCardNumber.add(textFieldCardNumber);
 		textFieldCardNumber.setColumns(10);
@@ -104,22 +115,24 @@ public class ATM {
 		buttonSuccess = new JButton("\u0413\u043E\u0442\u043E\u0432\u043E");
 		buttonSuccess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (textFieldCardNumber.getText().length() == 16){
-				inputNumberCard += textFieldCardNumber.getText();
-				System.out.println(inputNumberCard);
-				}
-				else{
-					JOptionPane.showMessageDialog(null, "Недостаточно цифр!");
+				if ((NumberChek(textFieldCardNumber.getText())) && (textFieldCardNumber.getText().length() == 3) ) {
+					inputNumberCard += textFieldCardNumber.getText();
+					System.out.println(inputNumberCard);
+				
+				} else  {
+
+					JOptionPane.showMessageDialog(null, "Неверный формат!", "Ошибка", JOptionPane.WARNING_MESSAGE);
+
 				}
 			}
 		});
 		buttonSuccess.setBounds(22, 69, 89, 23);
 		panelCardNumber.add(buttonSuccess);
 
-		JButton buttonClear = new JButton("\u041E\u0447\u0438\u0441\u0442\u0438\u0442\u044C");
+		buttonClear = new JButton("\u041E\u0447\u0438\u0441\u0442\u0438\u0442\u044C");
 		buttonClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				textFieldCardNumber.setText("");
 				inputNumberCard = "";
 				System.out.println(inputNumberCard + "success");
@@ -141,8 +154,9 @@ public class ATM {
 		pinPadPanel.setBackground(SystemColor.inactiveCaption);
 		pinPadPanel.setLayout(new GridLayout(4, 3));
 
-		// 0-9 buttons
+		// panelPinNumbers.add(pinPadPanel);
 
+		// 0-9 buttons
 		for (int i = 0; i < 9; i++) {
 			numberButtons[i] = new JButton(String.valueOf(i + 1));
 			numberButtons[i].addActionListener(new ActionListener() {
@@ -150,26 +164,24 @@ public class ATM {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// TODO Auto-generated method stub
-					if(!inputNumberCard.isEmpty()){
-					if (passwordField.getText().length() < 4) {
-						input += e.getActionCommand();
+					if (!inputNumberCard.isEmpty()) {
+						if (passwordField.getText().length() < 4) {
+							input += e.getActionCommand();
 
-						passwordField.setText(input);
-						System.out.println(input);
-					}
+							passwordField.setText(input);
+							System.out.println(input);
+						}
 
-				}
-					else {
-						JOptionPane.showMessageDialog(null, "Введите номер карты!");
+					} else {
+
+						JOptionPane.showMessageDialog(null, "Введите номер карты!", "Ошибка",
+								JOptionPane.WARNING_MESSAGE);
 					}
 				}
-				
 
 			});
 			pinPadPanel.add(numberButtons[i]);
 		}
-
-		// panelPinNumbers.add(pinPadPanel);
 
 		clearButton = new JButton("C");
 		clearButton.addActionListener(new ClearListener());
@@ -182,10 +194,16 @@ public class ATM {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// TODO Auto-generated method stub
-				if (passwordField.getText().length() < 4) {
-					input += e.getActionCommand();
-					passwordField.setText(input);
-					System.out.println(input);
+
+				if (!inputNumberCard.isEmpty()) {
+					if (passwordField.getText().length() < 4) {
+						input += e.getActionCommand();
+						passwordField.setText(input);
+						System.out.println(input);
+					}
+
+				} else {
+					JOptionPane.showMessageDialog(null, "Введите номер карты!");
 				}
 
 			}
@@ -194,7 +212,7 @@ public class ATM {
 
 		pinPadPanel.add(zeroButton);
 
-		enterButton = new JButton(/*"\u21B5"*/"Подтвердить");
+		enterButton = new JButton(/* "\u21B5" */"Подтвердить");
 		enterButton.addActionListener(new EnterListener());
 
 		pinPadPanel.add(enterButton);
@@ -204,7 +222,263 @@ public class ATM {
 		label.setBounds(33, 155, 122, 14);
 		panelAuthorisation.add(label);
 
-		// MainATM.pack();
+		initializeOperations();
+
+	}
+	
+	private boolean isValidCardNumber(){
+		String message = "";
+		if (!NumberChek(textFieldCardNumber.getText())) {
+		message = "Введите число";
+	
+		} 
+		else if (textFieldCardNumber.getText().length() != 16){
+			message = "Неверный формат!";
+			
+		}
+		else {
+			return true;
+		}
+		// сообщение пользователю
+		JOptionPane.showMessageDialog(null, message, "Ошибка", JOptionPane.WARNING_MESSAGE);
+		textFieldCardNumber.requestFocus();
+		textFieldCardNumber.selectAll();
+		//textFieldCardNumber.setText(null);
+		return false;
+		
+	}
+	
+
+	private boolean isValidDraw() {
+		String message = "";
+
+		// если не число
+		if (!NumberChek(textFieldSum.getText())) {
+			message = "Введите число";
+
+			// если < 10
+		} else if (Integer.parseInt(textFieldSum.getText()) < MIN_SUM) {
+			message = "Минимум для снятия: 100";
+
+			// если > 250
+		} else if (Integer.parseInt(textFieldSum.getText()) > MAX_SUM) {
+			message = "Максимум для снятия: 50000";
+
+			// если не кратно 10
+		} else if (Integer.parseInt(textFieldSum.getText()) % 100 != 0) {
+			message = "Сумма должна быть кратной 100";
+
+			// недостаточно средств
+		} // else if (Integer.parseInt(Balance) -
+			// Integer.parseInt(textFieldSum.getText()) < 0) {
+			// message = "Недостаточно средств";
+
+		// если все ок
+		else {
+			return true;
+		}
+
+		// сообщение пользователю
+		JOptionPane.showMessageDialog(null, message, "Ошибка", JOptionPane.WARNING_MESSAGE);
+		textFieldSum.requestFocus();
+		textFieldSum.selectAll();
+		textFieldSum.setText(null);
+		return false;
+	}
+
+	private void Draw() throws SQLException, Exception {
+		if (isValidDraw()) {
+			int money = Integer.parseInt(textFieldSum.getText());
+			
+			textAreaResult.setText(null);
+			textAreaResult.append("Вы сняли наличные в размере:" + "\n");
+			textAreaResult.append(textFieldSum.getText()+ "\n");
+			textAreaResult.append("Баланс:" + "\n");
+			printCheck("Снятие");
+			
+			/// int balance = Integer.parseInt(Balance);
+			// money = balance - money;
+			// String n = "Update password set balance=" + money + " where
+			/// login='" + Login + "'";
+			// SQLRequest(n);
+			// displayBalance();
+		}
+	}
+
+	private boolean isValidCash() {
+		String message = "";
+
+		// если не число
+		if (!NumberChek(textFieldSum.getText())) {
+			message = "Введите число";
+
+			// если < 100
+		} else if (Double.parseDouble(textFieldSum.getText()) < 100.0) {
+			message = "Минимум для пополнения: 100";
+
+			// если > 500
+		} else if (Double.parseDouble(textFieldSum.getText()) > 50000.0) {
+			message = "Максимум для пополнения: 50000";
+		} else if (Integer.parseInt(textFieldSum.getText()) % 100 != 0) {
+				message = "Сумма должна быть кратной 100";
+
+			// если все ок
+		} else {
+			return true;
+		}
+		JOptionPane.showMessageDialog(null, message, "Ошибка", JOptionPane.WARNING_MESSAGE);
+		textFieldSum.requestFocus();
+		textFieldSum.selectAll();
+		textFieldSum.setText(null);
+		return false;
+
+	}
+
+	private void Cash() throws SQLException, Exception {
+		if (isValidCash()) {
+			textAreaResult.setText(null);
+			int money = Integer.parseInt(textFieldSum.getText());
+			
+			textAreaResult.append("Вы внесли наличные в размере:" + "\n");
+			textAreaResult.append(textFieldSum.getText()+ "\n");
+			textAreaResult.append("Баланс:" + "\n");
+			printCheck("Внесение");
+			
+			
+			/*
+			 * int balance = Integer.parseInt(Balance); money = balance + money;
+			 * String n = "Update password set balance=" + money +
+			 * " where login='" + Login + "'"; SQLRequest(n); displayBalance();
+			 */
+		}
+	}
+
+	/// проверка строки на Integer
+	private boolean NumberChek(String num) {
+		try {
+			Integer.parseInt(num);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	private void initializeOperations() {
+		
+		panelOperations = new JPanel();
+		panelOperations.setBounds(0, 0, 421, 418);
+		MainATM.getContentPane().add(panelOperations);
+		panelOperations.setLayout(null);
+		
+		panelOperations.setVisible(false);
+		
+		
+
+		// scrollPaneInform.setColumnHeaderView(textAreaOut);
+
+		textAreaOutPut = new JTextArea();
+		textAreaOutPut.setEditable(false);
+		// scrollPaneInform.setRowHeaderView(textAreaOutPut);
+
+		scrollPaneInform = new JScrollPane(textAreaOutPut);
+		scrollPaneInform.setBounds(25, 27, 180, 219);
+		panelOperations.add(scrollPaneInform);
+		// panelOperations.add(label);
+		
+
+		textFieldSum = new JTextField();
+		textFieldSum.setBounds(225, 46, 184, 20);
+		panelOperations.add(textFieldSum);
+		textFieldSum.setColumns(10);
+
+		buttonClearOperation = new JButton("\u041E\u0447\u0438\u0441\u0442\u0438\u0442\u044C");
+		buttonClearOperation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textFieldSum.setText(null);
+			}
+		});
+		buttonClearOperation.setBounds(322, 77, 89, 29);
+		panelOperations.add(buttonClearOperation);
+
+		btnDrawOperation = new JButton(
+				"\u0421\u043D\u044F\u0442\u0438\u0435 \u043D\u0430\u043B\u0438\u0447\u043D\u044B\u0445");
+		btnDrawOperation.setBounds(25, 270, 175, 37);
+
+		panelOperations.add(btnDrawOperation);
+		btnDrawOperation.addActionListener(new DrawListener());
+
+		btnCashOperation = new JButton(
+				"\u0412\u043D\u0435\u0441\u0435\u043D\u0438\u0435 \u043D\u0430\u043B\u0438\u0447\u043D\u044B\u0445");
+		btnCashOperation.setBounds(25, 326, 180, 37);
+		panelOperations.add(btnCashOperation);
+		btnCashOperation.addActionListener(new CashListener());
+
+		JLabel label_Result = new JLabel("\u0420\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442:");
+		label_Result.setBounds(227, 152, 135, 14);
+		panelOperations.add(label_Result);
+
+		btnBalance = new JButton("\u0411\u0430\u043B\u0430\u043D\u0441");
+		btnBalance.setBounds(234, 326, 180, 37);
+		panelOperations.add(btnBalance);
+
+		textAreaResult = new JTextArea();
+		textAreaResult.setEditable(false);
+
+		scrollPaneResult = new JScrollPane(textAreaResult);
+		scrollPaneResult.setBounds(227, 177, 184, 69);
+		panelOperations.add(scrollPaneResult);
+
+		JLabel label_Sum = new JLabel("\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0441\u0443\u043C\u043C\u0443:");
+		label_Sum.setBounds(227, 28, 95, 14);
+		panelOperations.add(label_Sum);
+
+		JLabel label_3 = new JLabel("\u0427\u0435\u043A:");
+		label_3.setBounds(25, 11, 68, 14);
+		panelOperations.add(label_3);
+
+		JLabel label1 = new JLabel(
+				"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u0441\u0443\u043C\u043C\u0443 \u043E\u043F\u0435\u0440\u0430\u0446\u0438\u0438:");
+		label1.setBounds(227, 27, 172, 19);
+		label1.setFont(new Font("Tahoma", Font.PLAIN, 11));
+
+	}
+	
+	private void printCheck(String message){
+		textAreaOutPut.setText(null);
+		textAreaOutPut.setLineWrap(true);
+
+		textAreaOutPut.append("          ОАО <Банк>   " + "\n");
+
+		textAreaOutPut.append(message + " наличных"+ "\n"); // Или другая
+															// операция
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd         HH:mm:ss");
+		Date currentDate = new Date();
+		System.out.println(dateFormat.format(currentDate));
+
+		textAreaOutPut.append(dateFormat.format(currentDate) + "\n" + "\n");
+
+		textAreaOutPut.append("Сумма:" + "\n");
+
+		textAreaOutPut.append("Баланс карты:" + "\n");
+		textAreaOutPut.append("Комиссия:" + "0 RUB" + "\n");
+		textAreaOutPut.append("Номер карты:" + inputNumberCard +"\n");
+		textAreaOutPut.append("Номер операции:" + "\n");
+		textAreaOutPut.append("Банкомат:" + "\n" + "\n");
+
+		textAreaOutPut.append("***СПАСИБО!***");
+
+		System.out.println("ОАО <Банк>");
+		System.out.println("Выдача наличных");
+		// System.out.println(""); Дата Время????
+		System.out.println("Сумма:");
+		System.out.println("Баланс карты:");
+		System.out.println("Комиссия:" + "0 RUB");
+		System.out.println("Номер карты:");// добавить номер карты
+		System.out.println("Номер операции:");
+		System.out.println("Банкомат:");
+		System.out.println("Код авторизации:");
+		System.out.println("СПАСИБО!");
 	}
 
 	class ClearListener implements ActionListener {
@@ -223,26 +497,33 @@ public class ATM {
 
 	class EnterListener implements ActionListener {
 
-		@SuppressWarnings("deprecation")
+		// @SuppressWarnings("deprecation")
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
 
-			//counter--;
+			// counter--;
 			try {
 				if (counter != 0) {
 
 					if ((passwordField.getText().length() == 4) && (counter != 0)) {
+
 						counter--;
 
 						if (Authentication.authenticatePIN(input)) {
-							JOptionPane.showMessageDialog(null, "Успешная Авторизация!");
+							// JOptionPane.showMessageDialog(null, "Успешная
+							// Авторизация!");
+							JOptionPane.showMessageDialog(null, "Успешная Авторизация!", "Cообщение",
+									JOptionPane.INFORMATION_MESSAGE);
+							panelOperations.setVisible(true);
+							panelAuthorisation.setVisible(false);
 
 							System.out.println("Counter:" + counter);
 
 						} else {
 
-							JOptionPane.showMessageDialog(null, "Пин-код введен неверно! Введите еще раз!");
+							JOptionPane.showMessageDialog(null, "Пин-код введен неверно! Введите еще раз!", "Ошибка",
+									JOptionPane.WARNING_MESSAGE);
 
 							System.out.println("Counter:" + counter);
 							passwordField.setText("");
@@ -252,14 +533,15 @@ public class ATM {
 
 					}
 
-				} else if (passwordField.getText().length() == 4){
-					
+				} else if (passwordField.getText().length() == 4) {
+
 					passwordField.setText("");
 					input = "";
-					JOptionPane.showMessageDialog(null, "Вы превысили лимит попыток! Ваша карта заблокирована!");
-					counter = 2;
+
+					JOptionPane.showMessageDialog(null, "Вы превысили лимит попыток! Ваша карта заблокирована!",
+							"Ошибка", JOptionPane.WARNING_MESSAGE);
+					counter = 3;
 					enterButton.setEnabled(false);
-					
 
 				}
 
@@ -270,4 +552,39 @@ public class ATM {
 		}
 
 	}
+
+	class DrawListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			try {
+				Draw();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+	}
+
+	class CashListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			try {
+				Cash();
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
+
+	}
+
+	
 }
