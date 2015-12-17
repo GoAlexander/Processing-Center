12a087bc40223c3c508c96192b3c4ecc80d2a00e
Anchk420 <anchk420@bk.ru>
2015-12-17 21:55:54 +0300

    вызов интерфейса сайта из main

--- src/main.java
+++ src/main.java
@@ -21,7 +21,39 @@ public class main {
 			
 			int menu = in.nextInt();
 			if (menu==1) {
-				System.out.println("<Website interface> is not implemented yet.");
+				//System.out.println("<Website interface> is not implemented yet.");
+                                        try {
+                            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                                if ("Nimbus".equals(info.getName())) {
+                                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                                    break;
+                                }
+                            }
+                        } catch (ClassNotFoundException ex) {
+                            java.util.logging.Logger.getLogger(WebsiteInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                        } catch (InstantiationException ex) {
+                            java.util.logging.Logger.getLogger(WebsiteInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                        } catch (IllegalAccessException ex) {
+                            java.util.logging.Logger.getLogger(WebsiteInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+                            java.util.logging.Logger.getLogger(WebsiteInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                        }
+                        //</editor-fold>
+                        
+                        /* Create and display the form */
+                        java.awt.EventQueue.invokeLater(new Runnable() {
+                            public void run() {
+                                try {
+                                    new WebsiteInterface().setVisible(true);
+                                } catch (Exception e) {
+                                    // TODO Auto-generated catch block
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    }
+                }
+
 			}
 			else if (menu==2) {
 				System.out.println("<PaymentTerminal> is not implemented yet.");				
