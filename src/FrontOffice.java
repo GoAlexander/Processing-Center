78dabeb2cc08293e91f63880d269b15c726e744a
SokolovaNastya <xxxhactyaxxx@mail.ru>
2015-12-19 01:27:14 +0300

    Долгожданная комиссия

--- src/FrontOffice.java
+++ src/FrontOffice.java
@@ -51,80 +51,76 @@ public class FrontOffice {
 	
 	//methods
 	//Расчёт комиссии (task #6) (+Backoffice!)
-public static String commision(String cardNumber, String sum) {
-		int com = 0;
-                int min;
-                int max;
-                int percentage;
-                String cardN = cardNumber + "";
-		String cardNumb = cardN.substring(6, 9);
-                int diapasonCard = Integer.parseInt(cardNumb);
-                int sum = Integer.parseInt(sum);
-                if(diapasonCard >=1000 && diapasonCard <= 3000)
-                 {  
-                  percentage=1;
-                  min=10;
-                  max=100;
-                 }
-                 if(diapasonCard >=3001 && diapasonCard <= 6000)
-                  {
-                    percentage=2;
-                    min=10;
-                    max=110;
-                  }
-                 if(diapasonCard >=6001 && diapasonCard <= 7000)
-                  {
-                    percentage=3;
-                    min=0;
-                    max=0;
-                  }
-                 if(diapasonCard >=7001 && diapasonCard <= 9000)
-                  {
-                    percentage=4;
-                    min=12;
-                    max=100;
-                  }
-                 if(diapasonCard >=9001 && diapasonCard <= 9999)
-                  {
-                    percentage=5;
-                    min=10;
-                    max=100;
-                  }
-		if (min == 0 && max == 0) {
-			com = sum * (percentage * 0.01);
+	public static String commision(String cardNumber, String sum) {
+		Double com = 0.00;
+		Double min = 0.00;
+		Double max = 0.00;
+		int percentage = 0;
+		String cardNumb = cardNumber.substring(5, 9);
+		int diapasonCard = Integer.parseInt(cardNumb);
+		int summa = Integer.parseInt(sum);
+		if (diapasonCard >= 1000 && diapasonCard <= 3000) {
+			percentage = 1;
+			min = 10.0;
+			max = 100.0;
 		}
-		if (min == 0 && max != 0) {
-			com = sum * (percentage * 0.01);
+		if (diapasonCard >= 3001 && diapasonCard <= 6000) {
+			percentage = 2;
+			min = 10.0;
+			max = 110.0;
+		}
+		if (diapasonCard >= 6001 && diapasonCard <= 7000) {
+			percentage = 3;
+			min = 0.0;
+			max = 0.0;
+		}
+		if (diapasonCard >= 7001 && diapasonCard <= 9000) {
+			percentage = 4;
+			min = 12.0;
+			max = 100.0;
+		}
+		if (diapasonCard >= 9001 && diapasonCard <= 9999) {
+			percentage = 5;
+			min = 10.0;
+			max = 100.0;
+		}
+
+		if ((min.compareTo(0.0)==0) && (max.compareTo(0.0)==0)) {
+			com = summa * (percentage * 0.01);
+		}
+		if ((min.compareTo(0.0)==0) && (max.compareTo(0.0)!=0)) {
+			com = summa * (percentage * 0.01);
 			if (com < max) {
-				com=com;
+				com = com;
 			} else {
-				com=max;
+				com = max;
 			}
 		}
-		if (min != 0 && max == 0) {
-			com = sum * (percentage * 0.01);
+		if ((min.compareTo(0.0)!=0) && (max.compareTo(0.0)==0)) {
+			com = summa * (percentage * 0.01);
 			if (com < min) {
-				com=min;
+				com = min;
 			} else {
-				com=com;
+				com = com;
 			}
 		}
 
-		if (min != 0 && max != 0) {
-			com = sum * (percentage * 0.01);
+		if ((min.compareTo(0.0)!=0) && (max.compareTo(0.0)!=0)) {
+			com = summa * (percentage * 0.01);
 			if (min < com && com < max) {
-				com=com;
+				com = com;
 			} else {
+			
 				if (com < min) {
-					com=min;
+					com = min;
 				}
 				if (com > max) {
-					com=max;
-
+					com = max;
 				}
 			}
 		}
-           return com;
+		
+		return Double.toString(com);
 	}
 	/*
 	Sum commissionComputing(Sum oper_sum, int type_of_commission){
