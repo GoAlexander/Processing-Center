d9840250a9f54e69c505cffc2ed6f86b786d8165
Alexandra Lushkina <aleksandra-lushkina@yandex.ru>
2015-12-18 22:10:27 +0300

    Update GUITerminal.java

--- src/GUITerminal.java
+++ src/GUITerminal.java
@@ -53,7 +53,7 @@ public class GUITerminal {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 360);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
 		JPanel panCard = new JPanel();
