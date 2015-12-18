866ca33740a52525aef118b4705cf5fb47aafc09
qmandkili <qm2812@gmail.com>
2015-12-18 22:23:43 +0300

    Conversion

--- src/Converion.java
+++ src/Converion.java
@@ -5,22 +5,22 @@ public class Converion {
 		double dollar_exchange_rate = 70.9320471;
 		double euro_exchange_rate = 76.6420769;
 		if (typeFrom == 1 && typeTo == 2) {
-			double result = transaction * dollar_exchange_rate;
-			return result;
-		}
-		
-		if (typeFrom == 2 && typeTo == 1) {
 			double result = transaction / dollar_exchange_rate;
 			return result;
 		}
 		
+		if (typeFrom == 2 && typeTo == 1) {
+			double result = transaction * dollar_exchange_rate;
+			return result;
+		}
+		
 		if (typeFrom == 1 && typeTo == 3) {
-			double result = transaction * euro_exchange_rate;
+			double result = transaction / euro_exchange_rate;
 			return result;
 		}
 		
 		if (typeFrom == 3 && typeTo == 1) {
-			double result = transaction / euro_exchange_rate;
+			double result = transaction * euro_exchange_rate;
 			return result;
 		}
 		return 0;
